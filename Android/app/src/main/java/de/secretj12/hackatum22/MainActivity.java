35e2b01c8cac94b5baf6439f4d0f899398c5d397
oliver <ge96xey@mytum.de>
2022-11-20 01:27:15 +0100

    added lvl

--- Android/app/src/main/java/de/secretj12/hackatum22/MainActivity.java
+++ Android/app/src/main/java/de/secretj12/hackatum22/MainActivity.java
@@ -71,7 +71,6 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        setSupportActionBar(binding.toolbar);
 
 
         //load/initialize the osmdroid configuration, this can be done
